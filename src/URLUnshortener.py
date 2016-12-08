0955fe7789b4ed47f2f7cc4738a2903fb5fa16dd
Sam Bokai <onlineshrekjr@yahoo.com>
2016-12-08 12:34:36 +0100

    Add: scan comments for valid URLs using regex; Add: max_commentlength to configuration file

--- src/URLUnshortener.py
+++ src/URLUnshortener.py
@@ -1,9 +1,13 @@
+import re
 import requests.auth
 import praw
 import configparser
+from praw.models import MoreComments
 
 cfg_file = configparser.ConfigParser()
 cfg_file.read('url-unshortener.cfg')
+
+MAX_COMMENTLENGTH = int(cfg_file['urlunshortener']['max_commentlength'])
 
 APP_ID = cfg_file['reddit']['app_id']
 APP_SECRET = cfg_file['reddit']['app_secret']
@@ -23,12 +27,22 @@ access_token = response.json()['access_token']
 headers = {"Authorization": "bearer " + access_token, "User-Agent": USER_AGENT}
 response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
 
-print(response.json())
+print(response.json(), "\n\n\n\n\n\n\n\n\n\n\n\n")
 
+# Start PRAW Reddit Session
+reddit = praw.Reddit(user_agent=USER_AGENT, client_id=APP_ID, client_secret=APP_SECRET, username=REDDIT_ACCOUNT,
+                     password=REDDIT_PASSWD)
 
-# PRAW
+regex_pattern = re.compile(r'''(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.]
+[a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))
+*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))''')
 
-reddit = praw.Reddit(user_agent=USER_AGENT, client_id=APP_ID, client_secret=APP_SECRET)
+counter = 1
+allsubs = reddit.subreddit('all')
+for comment in allsubs.stream.comments():
+    if len(comment.body) < MAX_COMMENTLENGTH and regex_pattern.search(comment.body):
+        print("TEST NUMBER ", counter, "\nComment Length: ", len(comment.body), "\n\n", comment.body, "\n\n\n\n\n\n\n\n"
+                                                                                                      "\n\n\n\n\n\n\n\n\n\n")
+        counter += 1
 
-for submission in reddit.subreddit('funny').hot(limit=10):
-    print(submission.title)
+# (http(s)?:\ / \ /.)?(www\.)?[-a - zA - Z0 - 9 @: %._\+~  # =]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
